e8403b81372b3c262154475b9c52e0c565d88f73
theyugjain2005-debug <theyugjain2005@gmail.com>
2025-12-15 16:55:34 +0530

    Add files via upload

---
+++ task1.py
@@ -0,0 +1,14 @@
+a=int(input("Enter first number:"))
+b=int(input("Enter second number:"))
+sum=a+b
+difference=a-b
+product=a*b
+division=a/b
+print("The addition:",sum)
+print("The subtraction:",difference)
+print("The product:",product)
+print("The division:",division)
+
+
+
+
---
+++ task2.py
@@ -0,0 +1,3 @@
+a=input('enter first name:')
+b=input('enter last name:')
+print('hello,',a,b,'! welcome to python program.')
